120386328517c19fb6849a85504e4831b3850473
KemingChen <believe75467@gmail.com>
2015-07-14 12:28:22 +0800

    更新架構, Add PlayAudioTask class

--- app/src/main/java/ntut/edu/lab1323/mitrastar/service/handler/UploadAudioFileHandler.java
+++ app/src/main/java/ntut/edu/lab1323/mitrastar/service/handler/UploadAudioFileHandler.java
@@ -1,12 +1,8 @@
 package ntut.edu.lab1323.mitrastar.service.handler;
 
-import android.media.MediaPlayer;
-import android.util.Log;
-
 import org.eclipse.jetty.server.Request;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ntut.edu.lab1323.mitrastar.service.HttpBaseHandler;
+import ntut.edu.lab1323.mitrastar.service.task.PlayAudioTask;
 import ntut.edu.lab1323.mitrastar.view.MainActivity;
 
 
@@ -23,11 +20,11 @@ public class UploadAudioFileHandler extends HttpBaseHandler {
 
     @Override
     public void handle(final Request request, HttpServletRequest httpRequest, HttpServletResponse httpResponse, MainActivity activity) throws IOException {
-        final InputStream input = request.getInputStream();
+        InputStream input = request.getInputStream();
 
         Calendar calendar = Calendar.getInstance();
         File outputDir = activity.getCacheDir();
-        final File tempFile = File.createTempFile(Long.toString(calendar.getTime().getTime()), ".audio", outputDir);
+        File tempFile = File.createTempFile(Long.toString(calendar.getTime().getTime()), ".audio", outputDir);
         FileOutputStream fos = new FileOutputStream(tempFile);
 
         int length;
@@ -37,36 +34,15 @@ public class UploadAudioFileHandler extends HttpBaseHandler {
                 length = input.read(bytes);
                 if (length < 0)
                     break;
-                
-                fos.write(bytes, 0, length);
 
+                fos.write(bytes, 0, length);
             }
         } catch (IOException e) {
             e.printStackTrace();
-
         }
         fos.close();
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    MediaPlayer mediaPlayer = new MediaPlayer();
-
-                    FileInputStream fis = new FileInputStream(tempFile);
-                    Log.d("UploadAudioFileHandler", "delete " + tempFile.getName() + " -> " + Boolean.toString(tempFile.delete()));
-
-                    mediaPlayer.setDataSource(fis.getFD());
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-
-                }
-            }
-        });
-        thread.start();
+        (new PlayAudioTask(tempFile)).execute();
 
         httpResponse.setStatus(HttpServletResponse.SC_OK);
     }
