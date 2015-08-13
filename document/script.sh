25121d1e71290eadcb89a352648b4d2e0a6ee2e2
shung520 <ann141824@gmail.com>
2015-08-13 11:03:49 +0800

    update script for linux

--- document/script.sh
+++ document/script.sh
@@ -8,14 +8,18 @@ split_video_to_minuts=5
 
 # get video duration
 video_duration=$(ffmpeg -i $video_filename 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,//)
+video_time=$(ffprobe -i $video_filename -show_entries format=duration -v quiet -of csv="p=0")
+tmp=$(echo $video_time | awk '{print int($1+0.9999)}')
+split_num=$(echo $((tmp/(60*$split_video_to_minuts))) | awk '{print int($1+0.9)}')
 
 # convert ts to mp4
 ffmpeg -y -ss 00:00:00.00 -i $video_filename -t $video_duration -c:v copy -c:a copy $temp_ts_filename
 ffmpeg -y -i $temp_ts_filename -acodec copy -vcodec copy $temp_mp4_filename
 
-# # split video
-for i in $(seq 0 12)
+
+# split video
+for i in $(seq 1 $split_num)
 do
-	start_time=$[i*split_video_to_minuts*60]
-	ffmpeg -y -ss $start_time -i $temp_mp4_filename -t $[split_video_to_minuts*60] -c:v copy -c:a copy out_$i.mp4
+	start_time=$(((i-1)*split_video_to_minuts*60))
+	ffmpeg -y -ss $start_time -i $temp_mp4_filename -t $((split_video_to_minuts*60)) -c:v copy -c:a copy out_$i.mp4
 done
